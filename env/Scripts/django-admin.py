05ca91cc36f82f77ba2820ece29de42eedf5335a
Molly Clabots <molly.clabots@gmail.com>
2020-04-07 20:53:06 -0500

    Adding documenting files to project

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Molly Clabots\OneDrive - MNSCU\Documents\ITEC 2905\Project5\lmnop\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
