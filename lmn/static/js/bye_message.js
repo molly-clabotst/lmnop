0834c538e389f8c6b3261c37ae5023025bd5d1ce
Adetunji <adewuyia@oo.com>
2020-04-30 10:31:51 -0500

    Separate js to static folder

---
+++ lmn/static/js/bye_message.js
@@ -0,0 +1,6 @@
+
+
+// materialized css rounded class is applied to the toast
+//M.toast({ html: 'Succesfully logged out. Goodbye!', classes: 'rounded blue', dsiplayLength: 2000 });
+//location.href = "{% url 'lmn:homepage' %}" 
+
