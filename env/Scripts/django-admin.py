0f3323c76b53c66847d939d2d42653ca8572ea4f
Molly Clabots <molly.clabots@gmail.com>
2020-04-29 22:00:12 -0500

    Removing env and vscode settings from repo

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\molly clabots\onedrive - mnscu\documents\itec 2905\project5\lmnop\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
