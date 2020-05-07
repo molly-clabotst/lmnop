6d0ae580a19151619b15a58ad0478ac815565420
mekdes <magie>
2020-05-07 17:57:29 -0500

    update

--- lmn/views_notes.py
+++ lmn/views_notes.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Venue, Artist, Note, Show
-from .forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm
+from .forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm, UserSearchOwnNotesForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseForbidden
 
 
 
@@ -43,6 +44,18 @@ def notes_for_show(request, show_pk):   # pk = show pk
     return render(request, 'lmn/notes/note_list.html', { 'show': show, 'notes': notes } )
 
 
+
 def note_detail(request, note_pk):
     note = get_object_or_404(Note, pk=note_pk)
     return render(request, 'lmn/notes/note_detail.html' , { 'note': note })
+
+"""user can search fro their own note by specific title"""
+#@login_required
+def user_view_own_notes(request, user_pk):
+    #TODO if user has made a search, what di they search for ?
+    #usernotes = Note.objects.filter(user=user_pk).filter(by title).order_by('-posted_date')
+    user_search_title = UserSearchOwnNotesForm()
+    user = User.objects.get(pk=user_pk)
+    usernotes = Note.objects.filter(user=user.pk).order_by('-posted_date')
+    return render(request, 'lmn/notes/user_view_own_notes.html', { 'user': user , 'notes': usernotes, 'search_form': user_search_title }) 
+    
