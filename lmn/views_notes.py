ce0734d250392f0e92b50ab1713377c233d4656f
mekdes <magie>
2020-05-07 17:45:25 -0500

    update

--- lmn/views_notes.py
+++ lmn/views_notes.py
@@ -50,22 +50,13 @@ def note_detail(request, note_pk):
     return render(request, 'lmn/notes/note_detail.html' , { 'note': note })
 
 """user can search fro their own note by specific title"""
-@login_required
+#@login_required
 def user_view_own_notes(request, user_pk):
+    #TODO if user has made a search, what di they search for ?
+    #usernotes = Note.objects.filter(user=user_pk).filter(by title).order_by('-posted_date')
     user_search_title = UserSearchOwnNotesForm()
     user = User.objects.get(pk=user_pk)
     usernotes = Note.objects.filter(user=user.pk).order_by('-posted_date')
     return render(request, 'lmn/notes/user_view_own_notes.html', { 'user': user , 'notes': usernotes, 'search_form': user_search_title }) 
     
-    
-    # if request.method == 'POST':
-    #     pk = request.POST.get('pk')
-    #     note = get_object_or_404(Note, pk=pk)
-    #     print(note.user, request.user)
-    #     if note.user == request.user:
-    #         #note.show = True
-    #         note.save()
-    #     else:
-    #         return HttpResponseForbidden()
-    # return render(request, 'lmn/users/user_profile.html', {'note': note})
     
