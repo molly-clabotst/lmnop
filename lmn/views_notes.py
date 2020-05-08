fd0a5c1aa7d49834c288eb4720f357e1986eb5d9
mekdes <magie>
2020-05-08 15:40:01 -0500

    removed-unneccesary-code

--- lmn/views_notes.py
+++ lmn/views_notes.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Venue, Artist, Note, Show
-from .forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm, UserSearchOwnNotesForm
-
+from .forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -49,13 +48,13 @@ def note_detail(request, note_pk):
     note = get_object_or_404(Note, pk=note_pk)
     return render(request, 'lmn/notes/note_detail.html' , { 'note': note })
 
-"""user can search fro their own note by specific title"""
-#@login_required
-def user_view_own_notes(request, user_pk):
-    #TODO if user has made a search, what di they search for ?
-    #usernotes = Note.objects.filter(user=user_pk).filter(by title).order_by('-posted_date')
-    user_search_title = UserSearchOwnNotesForm()
-    user = User.objects.get(pk=user_pk)
-    usernotes = Note.objects.filter(user=user.pk).order_by('-posted_date')
-    return render(request, 'lmn/notes/user_view_own_notes.html', { 'user': user , 'notes': usernotes, 'search_form': user_search_title }) 
+# """user can search fro their own note by specific title"""
+# #@login_required
+# def user_view_own_notes(request, user_pk):
+#     #TODO if user has made a search, what di they search for ?
+#     #usernotes = Note.objects.filter(user=user_pk).filter(by title).order_by('-posted_date')
+#     user_search_title = UserSearchOwnNotesForm()
+#     user = User.objects.get(pk=user_pk)
+#     usernotes = Note.objects.filter(user=user.pk).order_by('-posted_date')
+#     return render(request, 'lmn/notes/user_view_own_notes.html', { 'user': user , 'notes': usernotes, 'search_form': user_search_title }) 
     
