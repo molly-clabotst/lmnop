95eb116fb6310e36e95e2bed5f8e90dec7792eac
mekdes <magie>
2020-05-08 20:11:36 -0500

    update

--- lmn/views_shows.py
+++ lmn/views_shows.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
 from .models import Note, Show
 
 from django.contrib.auth.decorators import login_required
@@ -9,8 +8,7 @@ from django.contrib.auth import authenticate
 
 """ view shows that have the most notes"""
 def show_with_most_notes(request):
-    shows = Show.objects.all().order_by('-show_date' )
+    shows = Show.objects.all().order_by('-show_date')
     print (shows)
     messages.info(request, f'the most note:{shows}')
-    #return render(request, 'lmn/notes/most_note_for_show.html')
     return render(request, 'lmn/notes/most_note_for_show.html', {'shows': shows})
