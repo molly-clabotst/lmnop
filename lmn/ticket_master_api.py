6ff24ebf1a61504d5850f4afb7b9e182ff740cfa
Andrew Dimery <43485223+Di30an@users.noreply.github.com>
2020-05-12 01:09:36 -0700

    Fixed intial error.

--- lmn/ticket_master_api.py
+++ lmn/ticket_master_api.py
@@ -80,7 +80,7 @@ def get_venue(venues):
         else:
             venue_city = venue['city']['name'] # finding value for the venue city
             venue_state = venue['state']['name'] # assigning value for the venue state
-            venue_statess = stateAbbrevation(venue_state)
+            venue_states = stateAbbrevation(venue_state)
             venue_new = Venue(name=venue_name, city=venue_city, state=venue_state) # creates a .models/Venue object and assigns the values
             venue_new.save() # Saves the object into the database
             print(venue_name,venue_city,venue_state)
